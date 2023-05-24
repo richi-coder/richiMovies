6f8375f2a97b5e6a14cc25879d2ccd41b2633f49
richi-coder <richicoder@gmail.com>
2023-05-24 00:45:09 -0400

    dynamic routes must be generated

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-
 function AdminPage() {
   return (
     <div>
         ADMINpage
         <p>
-            This page will be used for kind of showing up 'administration' things.
-            As NextJS recently introduced On Demand Static Regeneration, 
-            by clicking a button contained in this page, the /movies page content can be
-            updated in the top component which shows a 5 movies/carousel
+            AdminPAge Content
         </p>
     </div>
   )
