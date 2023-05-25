5836374d08c5cdfe925f6891736fa4c876dc37f8
richi-coder <richicoder@gmail.com>
2023-05-25 18:22:57 -0400

    appropiate linking from mobile menu, closing the menu

--- src/app/productions/MobileMenu.jsx
+++ src/app/productions/MobileMenu.jsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 import { FaSearch } from "react-icons/fa"
 
 function MobileMenu({ showMobileMenu, font }) {
-    // document.body.style.overflow = 'clip';
 
   return (
     <div className='fixed top-0 left-0 w-screen h-screen bg-black transition-all animate-fade-up'>
@@ -14,7 +13,9 @@ function MobileMenu({ showMobileMenu, font }) {
                 X
             </button>
             <div className='flex flex-col items-center w-full gap-5'>
-                <Link href={'/productions'}>
+                <Link
+                    onClick={showMobileMenu}
+                    href={'/productions'}>
                     <h1 className={`text-2xl ${font.className}`}>richiTrailers</h1>
                 </Link>
                 <Link href={'/search'} className="w-full bg-[rgb(24,24,27)] text-center">
@@ -23,9 +24,19 @@ function MobileMenu({ showMobileMenu, font }) {
                     </div>
                 </Link>
                 <div className="w-full">ACCOUNT</div>
-                <div className="w-full">HOME</div>
-                <div className="w-full">MOVIES</div>
-                <div className="w-full">SERIES</div>
+                <Link href={'/'} className="w-full">HOME</Link>
+                <Link
+                    onClick={showMobileMenu}
+                    href={'/productions/movie'}
+                    className="w-full">
+                    MOVIES
+                </Link>
+                <Link
+                    onClick={showMobileMenu}
+                    href={'/productions/tv'}
+                    className="w-full">
+                    SERIES
+                </Link>
             </div>
         </div>
     </div>
