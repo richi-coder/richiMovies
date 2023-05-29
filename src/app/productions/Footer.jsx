286c68df5535d4adea2f23ed1fe9dc71dac1daa3
richi-coder <richicoder@gmail.com>
2023-05-28 21:47:28 -0400

    fixing TMDB link target

--- src/app/productions/Footer.jsx
+++ src/app/productions/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className='bg-black px-12 sm:px-24 py-20'>
         <h5 className='pb-8 text-lg font-bold'>Credits</h5>
         <p>TMDB API usage</p>
-        <a href="https://www.themoviedb.org">The Movie Database Page</a>
+        <a href="https://www.themoviedb.org" target='_blank'>The Movie Database Page</a>
         <div className='text-xs text-gray-500'>This product uses the TMDB API but is not endorsed or certified by TMDB.</div>
         <div className='text-xs text-gray-500'>This is a demo app with no commercial purposes. Developed by richiCoder.</div>
     </div>
