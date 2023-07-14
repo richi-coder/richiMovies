3ed4c9f22fc0e6fca215f02fb9c68f7da2cc5b8c
richi-coder <richicoder@gmail.com>
2023-07-13 22:11:11 -0400

    fixing centering at not found feature

--- src/app/search/components/NotFound.jsx
+++ src/app/search/components/NotFound.jsx
@@ -1,8 +1,8 @@
 function NotFound() {
   return (
     <div className="flex flex-col items-center text-2xl mt-32">
-        <div className="font-bold">Not Found</div>
-        <div className="italic">Please try searching again!</div>
+        <div className="font-bold text-center">Not Found</div>
+        <div className="italic text-center">Please try searching again!</div>
     </div>
   )
 }
